a5cf9ec9b9e15ee8962423842d6b1be51bfc8b0e
ZolotukhinAS <>
2016-05-09 16:32:21 +0200

    ddd

--- CDS/Tables/dbo.names.sql
+++ CDS/Tables/dbo.names.sql
@@ -3,8 +3,8 @@ SET QUOTED_IDENTIFIER ON
 SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[names] (
-		[fname]     [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[DTIME]     [datetime] NULL
+		[fnames]     [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[DTIMEs]     [datetime] NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[names] SET (LOCK_ESCALATION = TABLE)
